4d41f36470cac4b99c68908d520b73b2f12fe85a
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-05-15 11:08:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,27 +24,35 @@ function renderProducts() {
 
 // Render cart list
 function renderCart() {
-	    if (sessionStorage.getItem('cartItems')!=null) {
-        const a=document.createElement('ul');
-     a.innerHTML=sessionStorage.getItem('cartItems');
-     a.setAttribute('id','cart-list')
-const cartList=document.getElementById('cart-list');
-cartList.replaceWith(a);  
-    }
+	   const cartList=document.getElementById('cart-list');
+    if (sessionStorage.getItem('cart')!=null) {
+        let a=JSON.parse(sessionStorage.getItem('cart'));
+        console.log(a);
+         a.forEach((prod,i)=>{
+            const li = document.createElement("li");
+            li.innerHTML = `${prod.name} - $${prod.price} <button onclick='removeFromCart(${prod.id})' class="remove-to-cart-btn" data-id="${prod.id}">Remove</button>`;
+        li.setAttribute('data-id',`${prod.id}`);
+        cartList.appendChild(li);
+    cpyarray.push(prod);
+         });
+         arr=[];
+   cpyarray.forEach((item)=>{
+if (item!=null) {
+    arr.push(item);}
+    });
 
+}
 }
 
 // Add item to cart
-let num=0;
 function addToCart(id) {
 	const cartList=document.getElementById('cart-list');
    const li = document.createElement("li");
    products.forEach((prod,i)=>{
     if (prod.id==id) {
-        li.innerHTML = `${prod.name} - $${prod.price} <button onclick='removeFromCart(${num})' class="remove-to-cart-btn" id=${num} data-id="${prod.id}">Remove</button>`;
-   li.setAttribute('id',`0${num}`);
+        li.innerHTML = `${prod.name} - $${prod.price} <button onclick='removeFromCart(${id})' class="remove-to-cart-btn" data-id="${prod.id}">Remove</button>`;
+        li.setAttribute('data-id',`${id}`);
         cartList.appendChild(li);
-    num++; 
     cpyarray.push(products[id-1]);
      } 
    });
@@ -54,18 +62,23 @@ if (item!=null) {
     arr.push(item);}
  });
    sessionStorage.setItem('cart',JSON.stringify(arr));
-   sessionStorage.setItem(`cartItems`,cartList.innerHTML);
-
 }
 
 // Remove item from cart
 function removeFromCart(a) {
-	const cartList=document.getElementById('cart-list');
+	let removecart=true;
+let removecart1=true;
+  //  const cartList=document.getElementById('cart-list');
     const remove=document.querySelectorAll(`#cart-list>li`);
    remove.forEach((item,i)=>{
-     if (item.getAttribute(`id`)==`0${a}`) {
-        item.remove();
-        delete cpyarray[a];
+     if (item.getAttribute(`data-id`)==a && removecart) {
+            item.remove(); 
+            removecart=false;  
+        cpyarray.forEach((elem,j)=>{       
+if (elem.id==a && removecart1) {
+    delete cpyarray[j];
+    removecart1=false;
+} });  
        }
     });
    arr=[];
@@ -76,8 +89,6 @@ if (item!=null) {
    });
     sessionStorage.clear();
     sessionStorage.setItem('cart',JSON.stringify(arr));
-    sessionStorage.setItem('cartItems',cartList.innerHTML);
-
 }
 
 // Clear cart
@@ -87,6 +98,8 @@ function clearCart() {
     item.remove();
   });
   sessionStorage.clear();
+  arr=[];
+  cpyarray=[];
 }
 
 // Initial render
